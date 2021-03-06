2345803e9aacb5167eeebb7f7914a5e1aa2f318f
mai <mai_saasaa@yahoo.com>
2021-03-06 02:01:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { render } from '@testing-library/react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div id="container"></div>
     </div>
   );
 }
 
-export default App;
+class Counter extends React.Component{
+  render (){
+    var textStyle={
+      fontSize: 72,
+      fontFamily:'sans-serif',
+      color:'#333',
+      fontWeight:'bold'
+    }
+    return (
+      <div style={textStyle}>
+        {this.props.display}
+      </div>
+    )
+  }
+}
+class CounterParent extends React.Component{
+constructor(props){
+  super(props);
+  this.state={
+    count:0
+  }
+  this.increase=this.increase.bind(this)
+}
+
+increase(e){
+  var currentCount = this.state.count;
+  if(e.shiftKey){
+    currentCount+=10;
+  } else {
+    currentCount+=1;
+  }
+  this.setState({
+    count: currentCount
+  });
+}
+  render(){
+var backgroundStyle={
+  padding:50,
+  backgroundColor:'#FFC53A',
+  width:250,
+  height:100,
+  borderRadius:10,
+  textAlign:'center'
+}
+ var buttonStyle={
+   fontSize:'1em',
+   width:30,
+   height:30,
+   fontFamily:'sans-serif',
+   color:'#333',
+   fontWeight:'bold',
+   lineHeight:'3px'
+ } 
+ return(
+   <div style={backgroundStyle}>
+<Counter display={this.state.count}/>
+<button style={buttonStyle} onClick={this.increase}>+</button>
+   </div>
+ )
+
+  }
+
+
+}
+
+export default CounterParent;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CounterParent from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CounterParent />
   </React.StrictMode>,
   document.getElementById('root')
 );
